d7c252de09bd10a6f76c6da479bf0375c7066adb
Philippe Joly <phil.joly2004@gmail.com>
2024-03-20 16:42:34 -0400

    api test

--- webApp/client/src/App.js
+++ webApp/client/src/App.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+    const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+    React.useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.dateStart));
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{!data ? "Loading..." : data}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- webApp/server/index.js
+++ webApp/server/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    axios({
+        method: "get",
+        url: "https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/demande.json",
+    })
+        .then((response) => {
+            res.send(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        });
 });
 
 app.listen(PORT, () => {
