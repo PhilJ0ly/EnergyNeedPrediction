c9131d847b79c90e5bd1ffd48f1b14c502e2b285
Philippe Joly <phil.joly2004@gmail.com>
2024-04-09 12:20:39 -0400

    corrected the fs read to send proper api response and added a prototype for mui x graph which does not work yet

--- webApp/client/src/App.js
+++ webApp/client/src/App.js
@@ -1,19 +1,66 @@
 import React from "react";
 import "./App.css";
+import { LineChart } from "@mui/x-charts";
 
 function App() {
     const [data, setData] = React.useState(null);
 
+    const keyToLabel = {
+        real: "Average Power Output (MW)",
+        gru: "GRU",
+        lstm: "LSTM",
+        rnn: "RNN",
+        scnn: "SCNN",
+        svr: "SVR",
+        dnn: "DNN",
+    };
+
+    const colors = {
+        real: "blue",
+        gru: "lightgreen",
+        lstm: "orange",
+        rnn: "red",
+        scnn: "pink",
+        svr: "yellow",
+        dnn: "darkgrey",
+    };
+
+    const customize = {
+        height: 300,
+        legend: { hidden: true },
+        margin: { top: 5 },
+    };
+
     React.useEffect(() => {
         fetch("/api")
             .then((res) => res.json())
-            .then((data) => setData(data));
+            .then((data) => setData(data))
+            .then((data) => console.log(data));
     }, []);
 
     return (
         <div className="App">
             <header className="App-header">
-                <p>{!data ? "Loading..." : data}</p>
+                {!data ? (
+                    <p>Loading...</p>
+                ) : (
+                    <LineChart
+                        xAxis={[
+                            {
+                                dataKey: "Date/Time",
+                                valueFormatter: (value) => value.toString(),
+                            },
+                        ]}
+                        series={Object.keys(keyToLabel).map((key) => ({
+                            dataKey: key,
+                            label: keyToLabel[key],
+                            color: colors[key],
+                            showMark: false,
+                        }))}
+                        dataset={data.data}
+                        {...customize}
+                    />
+                )}
             </header>
         </div>
     );
--- webApp/server/index.js
+++ webApp/server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const axios = require("axios");
-const { spawn } = require('child_process');
-const fs = require('fs').promises;
+const { spawn } = require("child_process");
+const fs = require("fs").promises;
+const fsNow = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,21 +10,43 @@ const app = express();
 app.get("/api", async (req, res) => {
     try {
         // Fetch data from Hydro-Québec API
-        const hydroResponse = await axios.get("https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/demande.json");
+        const hydroResponse = await axios.get(
+            "https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/demande.json"
+        );
         const hydroData = hydroResponse.data;
 
         const start = hydroData?.dateStart.split("T")[0];
         const end = hydroData?.recentHour.split("T")[0];
 
         const weather = await Promise.all([
-            { city: "MTL", wData: await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=45.28&longitude=-73.44&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`) },
-            { city: "QC", wData: await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=46.47&longitude=-71.23&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`) },
-            { city: "SHE", wData: await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=45.26&longitude=-71.41&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`) },
-            { city: "GAT", wData: await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=45.31&longitude=-75.33&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`) },
-        ])
+            {
+                city: "MTL",
+                wData: await axios.get(
+                    `https://api.open-meteo.com/v1/forecast?latitude=45.28&longitude=-73.44&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`
+                ),
+            },
+            {
+                city: "QC",
+                wData: await axios.get(
+                    `https://api.open-meteo.com/v1/forecast?latitude=46.47&longitude=-71.23&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`
+                ),
+            },
+            {
+                city: "SHE",
+                wData: await axios.get(
+                    `https://api.open-meteo.com/v1/forecast?latitude=45.26&longitude=-71.41&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`
+                ),
+            },
+            {
+                city: "GAT",
+                wData: await axios.get(
+                    `https://api.open-meteo.com/v1/forecast?latitude=45.31&longitude=-75.33&minutely_15=temperature_2m&start_date=${start}&end_date=${end}`
+                ),
+            },
+        ]);
 
         const weatherData = {};
-        
+
         weather.forEach(({ city, wData }) => {
             weatherData[city] = wData?.data?.minutely_15;
         });
@@ -32,48 +55,47 @@ app.get("/api", async (req, res) => {
         Object.entries(weatherData).forEach(([city, cityData]) => {
             cityData.time.forEach((timestamp, index) => {
                 if (!rearrangedData[timestamp]) {
-                rearrangedData[timestamp] = {};
+                    rearrangedData[timestamp] = {};
                 }
-                rearrangedData[timestamp][city] = cityData.temperature_2m[index];
+                rearrangedData[timestamp][city] =
+                    cityData.temperature_2m[index];
             });
         });
 
-        const wet = './data/wet.json';
-        const pow = './data/pow.json';
-        let out = './data/out.json';
+        const wet = "./data/wet.json";
+        const pow = "./data/pow.json";
+        let out = "./data/out.json";
         let pythonOutput;
 
         await fs.writeFile(wet, JSON.stringify(rearrangedData));
         await fs.writeFile(pow, JSON.stringify(hydroData));
 
-        const pythonProcess = spawn('python', ['./server/pythonProcess/main.py', pow, wet]);
+        const pythonProcess = spawn("python", [
+            "./server/pythonProcess/main.py",
+            pow,
+            wet,
+        ]);
 
-        pythonProcess.stderr.on('data', (data) => {
+        pythonProcess.stderr.on("data", (data) => {
             console.error(`Python error: ${data}`);
         });
 
-        // pythonProcess.stdout.on('data', (data) => {
-        //     out = data;
-        // });
-
-        pythonProcess.on('close', (code) => {
-            if(code == 0){
+        pythonProcess.on("close", (code) => {
+            if (code == 0) {
                 //read from out.json
-                fs.readFile(out, (err, data) => {
-                    if(err) {
+                fsNow.readFile(out, "utf8", (err, data) => {
+                    if (err) {
                         console.error(err);
-                        return
+                        return;
                     }
-                    pythonOutput = JSON.parse(data.toString())
-                })
+                    pythonOutput = JSON.parse(data);
+                    res.send(pythonOutput);
+                    //delete files
+                    //fs.unlink(wet);
+                    //fs.unlink(pow);
+                    //fs.unlink(out);
+                });
             }
-
-            res.send(pythonOutput);
-
-            //delete files
-        //     fs.unlink(wet);
-        //     fs.unlink(pow);
-        // //     fs.unlink(out);
         });
     } catch (error) {
         console.error(error);
